7ccc73263cdaaa17532c6ca0559cfc1d35432b32
joseg-alvesg <cinhoalves85@gmail.com>
2022-12-04 13:24:44 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ class App extends React.Component {
   constructor() {
     super();
 
-    this.onInputChange = this.onInputChange.bind(this); // give power to see thisasdfasd
-
     this.state = {
       cardName: '',
       cardDescription: '',
@@ -17,6 +15,7 @@ class App extends React.Component {
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
+      isSaveButtonDisabled: true,
     };
   }
 
@@ -24,7 +23,40 @@ class App extends React.Component {
     const { name, value, type, checked } = event.target;
     this.setState({
       [name]: type === 'checkbox' ? checked : value,
-    });
+    }, this.disableBtn);
+  };
+
+  disableBtn = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    } = this.state;
+
+    const maxNumber = 90;
+    const attr1 = Number(cardAttr1) >= 0 && Number(cardAttr1) <= maxNumber;
+    const attr2 = Number(cardAttr2) >= 0 && Number(cardAttr2) <= maxNumber;
+    const attr3 = Number(cardAttr3) >= 0 && Number(cardAttr3) <= maxNumber;
+    const attrTotal = attr1 && attr2 && attr3;
+
+    const name = cardName !== '';
+    const descript = cardDescription !== '';
+    const image = cardImage !== '';
+    const rarity = cardRare !== '';
+    const stringers = name && descript && image && rarity;
+
+    const maxSum = 210;
+    const sum = (Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3)) <= maxSum;
+
+    if (sum && attrTotal && stringers) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   };
 
   render() {
